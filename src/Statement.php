566cff3e2a9c9c49084ff9921177beb71bfa8321
dlinckd <dlinck.d@gmail.com>
2025-11-29 14:19:14 +0100

    Added Statement interface to Statement class

--- src/Statement.php
+++ src/Statement.php
@@ -6,8 +6,9 @@ namespace Xwero\IdableQueriesRelational;
 
 use PDO;
 use PDOStatement;
+use Xwero\IdableQueriesCore\Statement as StatementInterface;
 
-class Statement extends PDOStatement
+class Statement extends PDOStatement implements StatementInterface
 {
     protected function __construct(public readonly PDO $client)
     {}
